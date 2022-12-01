0107eef5f2dc4fd43b8a1ad4678c61af7e330528
Yadid Hadad <yadidhadad@gmail.com>
2022-12-01 11:13:04 +0200

    yadid update

--- src/cmps/board-menu.vue
+++ src/cmps/board-menu.vue
@@ -1,9 +1,14 @@
 <template>
     <section class="board-menu flex column" :class="{ menuIsHidden: menuIsHidden }">
-        <button class="btn" @click="toggleBoardMenu">
-            <span class="fa-solid x-icon"></span>
-        </button>
-        <span class="title">Menu</span>
+        <div class="flex justify-between">
+            <button class="btn" @click="toggleBoardMenu">
+                <span class="fa-solid x-icon"></span>
+            </button>
+            <span class="title">Menu</span>
+            <button class="btn" @click="toggleBoardMenu">
+                <span class="fa-solid x-icon"></span>
+            </button>
+        </div>
 
         <button v-for="btn in btns" class="btn">
             <span :class="btn.icon"></span>
@@ -13,35 +18,34 @@
 </template>
 <script>
 export default {
-    name: 'board-menu',
-    props: ['menuIsHidden'],
+    name: "board-menu",
+    props: ["menuIsHidden"],
     components: {},
     created() { },
     data() {
         return {
             btns: [
                 {
-                    txt: 'About this board',
-                    icon: 'fa-brands trello-icon'
+                    txt: "About this board",
+                    icon: "fa-brands trello-icon",
                 },
                 {
-                    txt: 'Change background',
-                    icon: 'fa-regular user-icon'
+                    txt: "Change background",
+                    icon: "fa-regular user-icon",
                 },
                 {
-                    txt: 'Activity',
-                    icon: 'fa-solid settings-icon'
+                    txt: "Activity",
+                    icon: "fa-solid settings-icon",
                 },
-            ]
-        }
+            ],
+        };
     },
     methods: {
         toggleBoardMenu() {
-            console.log('A')
-            this.$emit('toggleBoardMenu', this.toggleBoardMenu)
-
-        }
+            console.log("A");
+            this.$emit("toggleBoardMenu", this.toggleBoardMenu);
+        },
     },
     computed: {},
-}
+};
 </script>
