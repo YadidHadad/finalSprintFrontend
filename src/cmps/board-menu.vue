728cad9058798abbf38ec083370c197ca5a0060a
Yadid Hadad <yadidhadad@gmail.com>
2022-12-01 18:26:26 +0200

    yadid update

--- src/cmps/board-menu.vue
+++ src/cmps/board-menu.vue
@@ -1,84 +1,86 @@
 <template>
-    <section class="board-menu flex column" :class="{ menuIsHidden: menuIsHidden }">
-        <section class="title flex justify-between row">
-            <button class="btn" @click="toggleBoardMenu">
-                <span class="fa-solid arrow-icon"></span>
-            </button>
-            <span>Menu</span>
-            <button class="btn" @click="toggleBoardMenu">
-                <span class="fa-solid x-icon"></span>
-            </button>
-        </section>
-        <section class="menu-container actions">
-            <section class="nav-btns flex column">
-                <button class="btn">
-                    <span class="fa-brands trello-icon"></span>
-                    <span>About this board</span>
+    <section class="board-menu" :class="{ menuIsHidden: menuIsHidden }">
+        <section v-if="!showBGCMenu" class="board-menu-page  flex column">
+            <section class="title flex justify-between row">
+                <button class="btn" @click="toggleBoardMenu">
+                    <!-- <span class="fa-solid arrow-icon"></span> -->
                 </button>
-                <button class="btn" @click="this.showBGCMenu = !this.showBGCMenu">
-                    <span class="fa-regular img-icon"></span>
-                    <span>Change background</span>
+                <span>Menu</span>
+                <button class="btn" @click="toggleBoardMenu">
+                    <span class="fa-solid x-icon"></span>
                 </button>
             </section>
 
-            <section class="activities">
-                <button class="btn ">
-                    <span class="fa-solid act-icon"></span>
-                    <span>Activity</span>
-                </button>
-                <div class="activities-container">
-                    <div v-for="activity in activities" class="activity flex row align-start">
-                        <div class="activity">
-                            <span class="btn flex row align-baseline align-center justify-center">{{
-                                    getInitials(activity.byMember.fullname)
-                            }}</span>
-                        </div>
-                        <div class=" flex column justify-start">
-                            <div>
-                                <span class="fullname">{{ activity.byMember.fullname + ' ' }}</span>
-                                <span>{{ activity.txt }}</span>
+            <section class="menu-container actions">
+                <section class="nav-btns flex column">
+                    <button class="btn">
+                        <span class="fa-brands trello-icon"></span>
+                        <span>About this board</span>
+                    </button>
+                    <button class="btn" @click="this.showBGCMenu = !this.showBGCMenu">
+                        <span class="fa-regular img-icon"></span>
+                        <span>Change background</span>
+                    </button>
+                </section>
+
+                <section class="activities">
+                    <button class="btn ">
+                        <span class="trellicons activity-icon"></span>
+                        <span>Activity</span>
+                    </button>
+                    <div class="activities-container">
+                        <div v-for="activity in activities" class="activity flex row align-start">
+                            <div class="activity">
+                                <span class="btn flex row align-baseline align-center justify-center">{{
+                                        getInitials(activity.byMember.fullname)
+                                }}</span>
                             </div>
-                            <span>{{ getTimeAgo(activity.createdAt) }}</span>
+                            <div class=" flex column justify-start">
+                                <div>
+                                    <span class="fullname">{{ activity.byMember.fullname + ' ' }}</span>
+                                    <span>{{ activity.txt }}</span>
+                                </div>
+                                <span>{{ getTimeAgo(activity.createdAt) }}</span>
+                            </div>
                         </div>
+                    </div>
+                </section>
+            </section>
+        </section>
+
+        <section v-if="showBGCMenu" class="board-menu-page  flex column">
+            <section class="title flex justify-between row">
+                <button class="btn" @click="this.showBGCMenu = !this.showBGCMenu">
+                    <span class="fa-solid arrow-icon"></span>
+                </button>
+                <span>Change background</span>
+                <button class="btn" @click="toggleBoardMenu">
+                    <span class="fa-solid x-icon"></span>
+                </button>
+            </section>
+            <section>
+                <div class="  flex row align-center justify-between">
+                    <div class=" photos flex column align-center">
+                        <div class="btn">
+                        </div>
+                        <span>Photos</span>
+                    </div>
+                    <div class="colors flex column  align-center">
+                        <div class="btn">
+                        </div>
+                        <span>Colors</span>
                     </div>
                 </div>
             </section>
+            <section>
+                <span>Costume</span>
 
-        </section>
-    </section>
-
-    <section v-if="showBGCMenu" class="board-menu flex column">
-        <section class="title flex justify-between row">
-            <button class="btn" @click="this.showBGCMenu = !this.showBGCMenu">
-                <span class="fa-solid arrow-icon"></span>
-            </button>
-            <span>Change background</span>
-            <button class="btn" @click="toggleBoardMenu">
-                <span class="fa-solid x-icon"></span>
-            </button>
-        </section>
-        <section>
-            <div class="  flex row align-center justify-between">
-                <div class=" photos flex column align-center">
-                    <div class="btn">
+                <div class="costume flex column ">
+                    <div class="btn flex align-center justify-center">
+                        <span class="fa-regular plus-icon"></span>
                     </div>
-                    <span>Photos</span>
                 </div>
-                <div class="colors flex column  align-center">
-                    <div class="btn">
-                    </div>
-                    <span>Colors</span>
-                </div>
-            </div>
-        </section>
-        <section>
-            <span>Costume</span>
-
-            <div class="costume flex column ">
-                <div class="btn flex align-center justify-center">
-                    <span class="fa-regular plus-icon"></span>
-                </div>
-            </div>
+            </section>
         </section>
     </section>
 </template>
