c499690ceded669fc0c3862a4fd30c67818d7c9b
Amir Gombo <amir.gomb4@gmail.com>
2022-12-03 11:44:29 +0200

    amir

--- src/cmps/dates-preview.vue
+++ src/cmps/dates-preview.vue
@@ -23,14 +23,17 @@ export default {
     methods: {
         toggleIsComplete() {
             this.isComplete = !this.isComplete
-            this.$emit('markComplete' , this.isComplete)
+            this.$emit('markComplete', this.isComplete)
         }
     },
     computed: {
         dueDateStr() {
-            const dueDateStr = new Date(this.$store.getters.getEditedTask.dueDate).toLocaleTimeString('en-GB').slice(0, 5)
-            console.log(dueDateStr, ',,,,,,,,,,,,,,,,,,,,,,,,,,,,');
-            return dueDateStr
+            const dueDateStr = new Date(this.$store.getters.getEditedTask.dueDate)
+            const dueTime = dueDateStr.toLocaleTimeString('en-GB').slice(0, 5)
+            const dueDate = `${dueDateStr.getDate()}/${dueDateStr.getMonth() + 1}`
+
+            console.log(dueTime, ',,,,,,,,,,,,,,,,,,,,,,,,,,,,');
+            return dueDate + ' '  + dueTime
         },
         dueDateMs() {
             return this.$store.getters.getEditedTask.dueDate
