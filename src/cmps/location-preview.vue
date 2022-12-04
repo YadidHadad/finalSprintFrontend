f32e721ebb751e86caec7834179af3e0482b1477
Amir Gombo <amir.gomb4@gmail.com>
2022-12-04 09:01:29 +0200

    amir

--- src/cmps/location-preview.vue
+++ src/cmps/location-preview.vue
@@ -4,7 +4,7 @@
 
         <GoogleMap api-key="AIzaSyAV_zVjXKofPeV5nwAzfNKJ7Gt4VEMjHOw" style="width: 100%; height: 100%" :center="center"
             :zoom="10">
-            <Marker v-for="m in markers" :options="m" @click="center = m.position" />
+            <Marker v-for="(m ,index) in markers" :options="m" @click="center = m.position" :key="index" />
         </GoogleMap>
 
     </section>
@@ -24,25 +24,12 @@ export default {
         return {
             center: {},
             markers: [
-                // {
-                //     title: 'Tel-Aviv',
-                //     label: 'Hi!',
-                //     position: { lat: 32.0853, lng: 34.7818 },
-                // },
-                // {
-                //     title: 'Jerusalem',
-                //     position: { lat: 31.7683, lng: 35.2137 },
-                // },
-                // {
-                //     title: 'Rehovot',
-                //     position: { lat: 31.8928, lng: 34.8113 },
-                // },
             ],
         }
     },
     created() {
         this.center =  this.location
-        console.log(this.center , this.location , '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+        console.log(this.center , this.location , '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
         const marker = {
             title: 'Tel-Aviv',
             label: 'Hi!',
