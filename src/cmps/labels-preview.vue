71e870f4317926f148548dad9c2ca2f230dec87d
Yadid Hadad <yadidhadad@gmail.com>
2022-12-05 16:45:47 +0200

    yadid labels fix

--- src/cmps/labels-preview.vue
+++ src/cmps/labels-preview.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="labels-preview  flex column">
         <h4 class="title">Labels</h4>
-        <section class="labels-container flex row align-center justify-center">
-            <div v-for="(label, i) in taskLabels" :key="i" :style="{ backgroundColor: label.color }"
-                class="label-preview flex row align-center justify-center">
+        <section class="labels-container flex row align-center justify-start gap5 wrap">
+            <div v-for="(label, i) in taskLabels" :key="i" :style="{ backgroundColor: rgbaColors[label.id] }"
+                class="label-preview flex row align-center justify-start">
                 <div :style="{ backgroundColor: label.color }" class="color-circle"></div>
                 <span>{{ label.title }}</span>
             </div>
@@ -16,11 +16,32 @@
 
 <script>
 export default {
+    data() {
+        return {
+            rgbaColors: {},
+        }
+    },
+    methods: {
+        hexToRgbA(hex) {
+            console.log(hex)
+            var c;
+            c = hex.substring(1).split('');
+            if (c.length == 3) {
+                c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+            }
+            c = '0x' + c.join('');
+            return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.5)';
+        },
+    },
     computed: {
         taskLabels() {
-            // console.log(this.$store.getters.taskLabels);
-            return this.$store.getters.taskLabels
-        }
+            const labels = this.$store.getters.taskLabels
+            labels.forEach(label => {
+                console.log(label.color)
+                this.rgbaColors[label.id] = this.hexToRgbA(label.color)
+            })
+            return labels
+        },
     }
 }
 </script>
